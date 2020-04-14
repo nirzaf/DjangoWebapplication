393cf5b82e5f145b68d8598f2ea837f4954cdfef
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-04-15 02:24:10 +0530

    Add project files.

---
+++ Doc99/app/views.py
@@ -0,0 +1,45 @@
+"""
+Definition of views.
+"""
+
+from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpRequest
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        }
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Contact Us',
+            'message':'Please find our contact details below',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'About Us',
+            'message':'This is a About Page',
+            'year':datetime.now().year,
+        }
+    )
